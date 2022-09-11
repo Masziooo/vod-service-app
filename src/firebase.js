b751aee319afbcd04828084b1a403603eb16c2ac
Masziooo <maciekborys.olszewski@gmail.com>
2022-09-11 12:13:19 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWFgBT9Skk-GmL-QZ3eHdTJfHj0LK7hzo",
-  authDomain: "netflix-project-203aa.firebaseapp.com",
-  projectId: "netflix-project-203aa",
-  storageBucket: "netflix-project-203aa.appspot.com",
-  messagingSenderId: "187738021860",
-  appId: "1:187738021860:web:938ed6eff8d3b87b32b1f1",
+  hidden.env
 };
 
 // Initialize Firebase
